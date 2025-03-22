0b5e448b2e2ba6c4ac17e75d061b31216d5d458e
vyacheslavbotoev <botoevslava27032003@mail.ru>
2025-03-22 18:15:12 +0300

    added profile update: update name, email and two factor status

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,36 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
-import { UserService } from './user.service';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Patch } from '@nestjs/common'
+import { UserService } from './user.service'
 import { Authorized } from '../auth/decorators/authorized.decorator'
 import { Authorizaation } from '../auth/decorators/auth.decorator'
 import { UserRole } from '../../prisma/__generated__'
+import { UpdateUserDto } from '@/user/dto/update-user.dto'
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
 
-  @Authorizaation()
-  @HttpCode(HttpStatus.OK)
-  @Get('profile')
-  public async findProfile(@Authorized('id') userId: string) {
-    return this.userService.findById(userId)
-  }
+	@Authorizaation()
+	@HttpCode(HttpStatus.OK)
+	@Get('profile')
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId)
+	}
 
-  @Authorizaation(UserRole.ADMIN)
-  @HttpCode(HttpStatus.OK)
-  @Get('by-id/:id')
-  public async findById(@Param('id') userId: string) {
-    return this.userService.findById(userId)
-  }
+	@Authorizaation(UserRole.ADMIN)
+	@HttpCode(HttpStatus.OK)
+	@Get('by-id/:id')
+	public async findById(@Param('id') userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Authorizaation()
+	@HttpCode(HttpStatus.OK)
+	@Patch('profile')
+	public async updateProfile(
+		@Authorized('id') userId: string,
+		@Body() dto: UpdateUserDto)
+	{
+		return this.userService.update(userId, dto)
+	}
 }
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from '../prisma/prisma.service'
 import { AuthMethod } from '../../prisma/__generated__'
 import { hash } from 'argon2'
+import { UpdateUserDto } from '@/user/dto/update-user.dto'
 
 @Injectable()
 export class UserService {
@@ -62,5 +63,22 @@ export class UserService {
 		})
 
 		return user
+	}
+
+	public async update(userId: string, dto: UpdateUserDto) {
+		const user = await this.findById(userId)
+
+		const updatedUser = await this.prismaService.user.update({
+			where: {
+				id: user.id,
+			},
+			data: {
+				email: dto.email,
+				displayName: dto.name,
+				isTwoFactorEnabled: dto.isTwoFactorEnabled
+			}
+		})
+
+		return updatedUser
 	}
 }
